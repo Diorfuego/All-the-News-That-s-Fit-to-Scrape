ec3516767176e80ebf2d30698e58351adf00ced9
Dior <49665409+Diorfuego@users.noreply.github.com>
2019-12-08 23:11:27 -0800

    Update note.js

--- All/models/note.js
+++ All/models/note.js
@@ -1,1 +1,21 @@
-test
+  
+var mongoose = require("mongoose");
+
+// Savesto the Schema constructor
+var Schema = mongoose.Schema;
+
+// Create a new NoteSchema object
+
+var NoteSchema = new Schema({
+  // 
+  title: String,
+
+  body: String
+});
+
+// Creates our model from the above schema
+var Note = mongoose.model("Note", NoteSchema);
+
+
+// Exports the Note model
+module.exports = Note;
